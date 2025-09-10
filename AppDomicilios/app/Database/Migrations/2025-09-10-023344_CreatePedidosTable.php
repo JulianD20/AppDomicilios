05fa5da10c027cecac8a1e2600948fdcac37ea33
Andrés Aguirre <andresramos123@outlook.com>
2025-09-09 21:37:16 -0500

    Migrates Pedidos

---
+++ AppDomicilios/app/Database/Migrations/2025-09-10-023344_CreatePedidosTable.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreatePedidosTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'domiciliario_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'cuadrante_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'monto' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '10,2',
+                'default'    => 0.00,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addKey('domiciliario_id');
+        $this->forge->addKey('cuadrante_id');
+
+        // Claves foráneas (requiere InnoDB)
+        $this->forge->addForeignKey('domiciliario_id', 'domiciliarios', 'id', 'RESTRICT', 'RESTRICT');
+        $this->forge->addForeignKey('cuadrante_id', 'cuadrantes', 'id', 'RESTRICT', 'RESTRICT');
+
+        $this->forge->createTable('pedidos');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('pedidos', true);
+    }
+}
